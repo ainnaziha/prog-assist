8c5c451bce3b2d5db519968d43b57276e08434bb
ainnaziha <ainnaziha551@gmail.com>
2023-11-10 19:04:16 +0800

    feat: history api

--- app/api/assessment/history/route.ts
+++ app/api/assessment/history/route.ts
@@ -1,32 +1,35 @@
 import { NextResponse } from 'next/server';
 import { AssessmentResult } from '@/lib/models/contents';
+import { getUser } from '@/lib/auth';
+import executeQuery from '@/lib/db';
 
 export async function GET(request: Request) 
 {  
   try {
-    const result: AssessmentResult[] = [
-      {
-          id: 1,
-          category: 'Frontend',
-          date: '10 Nov 2023',
-          recommendation: 'need to fix the teeth'
-      },
-      {
-        id: 1,
-        category: 'Backend',
-        date: '10 Nov 2023',
-        recommendation: 'need to fix the teeth'
-      },
-      {
-        id: 1,
-        category: 'Framework',
-        date: '10 Nov 2023',
-        recommendation: 'need to fix the teeth'
-      } 
-    ];
+    const user = await getUser();
+
+    if (user === null) {
+      return NextResponse.json({ message: 'Unauthorized access' }, { status: 401 });
+    }
+
+    const query = 'SELECT * FROM results WHERE user_id = ? ORDER BY id DESC LIMIT 10';
+    const values = [user.id];
+
+    const result = await executeQuery({ query, values });
+    
+    const assessments = result.map(item => {
+      let date = new Date(item.created_at);
+   
+      return {
+          id: item.id,
+          category: item.type,
+          date: `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`,
+          recommendation: item.recommendation
+      }
+    });
     
     return NextResponse.json({ 
-      data: result 
+      data: assessments 
     });
    } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
