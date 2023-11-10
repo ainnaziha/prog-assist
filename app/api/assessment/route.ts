2c65733ae4654520f7f23cfffe95fafa34957a57
muza <muzallyefah@gmail.com>
2023-11-10 16:22:57 +0800

    fix: api output to json

--- app/api/assessment/route.ts
+++ app/api/assessment/route.ts
@@ -9,22 +9,36 @@ export async function POST(request: Request)
   try {
     const data: AssessmentRequest = await request.json();
 
-    // const response = await inference.textGeneration({
-    //   model: process.env.HF_MODEL,
-    //   inputs: generatePrompt(category ?? ''),
-    //   parameters: {
-    //     max_new_tokens: 500,
-    //     // @ts-ignore
-    //     typical_p: 0.2,
-    //     repetition_penalty: 1,
-    //     return_full_text: false,
-    //   },
-    // });
+    const response = await inference.textGeneration({
+      model: process.env.HF_MODEL,
+      inputs: generatePrompt(data.type, JSON.stringify(data.questions)),
+      parameters: {
+        max_new_tokens: 500,
+        // @ts-ignore
+        typical_p: 0.2,
+        repetition_penalty: 1,
+        return_full_text: false,
+      },
+    });
 
-    //let jsonObject = JSON.parse(response.generated_text);
+    let keys = ["score", "compatibility", "recommendation"];
+    let values = [];
+    let str = response.generated_text;
+    for (let i = 0; i < keys.length; i++) {
+      let start = str.indexOf(keys[i]) + keys[i].length + 2;
+      let end = i < keys.length - 1 ? str.indexOf(keys[i + 1]) : undefined;
+      let value = str.substring(start, end).trim();
+      values.push(value);
+    }
+
+    let jsonStr = `{
+      "score": "${values[0]}",
+      "compatibility": "${values[1]}",
+      "recommendation": "${values[2]}"
+    }`;
 
     return NextResponse.json({ 
-      data: data,
+      data: JSON.parse(jsonStr),
     });
    } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
--- lib/ai-helper.ts
+++ lib/ai-helper.ts
@@ -2,15 +2,14 @@ import { HfInference } from "@huggingface/inference";
 
 export const inference = new HfInference(process.env.HF_ACCESS_TOKEN);
 
-export function generatePrompt(category: string): string {
-    return `<|prompter|>
-    I need a set of 5 questions for a user assessment in the category of ${category}. The questions should cover the score, compatibility, and recommendations to improve. The answers should be provided in multiple-choice format. Please format the output only in JSON below.
-    [
-        {
-            question: str
-            options: []
-        }
-    ]
-     <|endoftext|><|assistant|>
-    `;
+export function generatePrompt(category: string, questions: string): string {
+    return `<|prompter|>Assess the user's proficiency in ${category} development using the given answers. 
+Question: ${questions}
+The questions cover aspects such as proficiency level, compatibility with frameworks/tools, and areas for improvement. Please provide an assessment result in the following format:
+{
+    "score": "",
+    "compatibility": "",
+    "recommendation": ""
+}
+<|endoftext|><|assistant|>`;
 }
